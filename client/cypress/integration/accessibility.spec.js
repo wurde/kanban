a43b513d26ee1bad0d23564df1391f7accbd67ed
Andy Bettisworth <andy@accreu.com>
2019-12-13 17:28:03 -0600

    Update test

--- client/cypress/integration/accessibility.spec.js
+++ client/cypress/integration/accessibility.spec.js
@@ -11,13 +11,5 @@ describe('Accessibility', () => {
     cy.get('body').should('has.css', 'background-color', 'rgba(0, 0, 0, 0)');
 
     cy.get('h6').should('have.css', 'color', 'rgb(0, 0, 0)');
-
-    cy.get('.card')
-      .should('have.css', 'background-color', 'rgb(255, 243, 205)')
-      .should(
-        'have.css',
-        'box-shadow',
-        'rgba(77, 99, 119, 0.47) 0px 2px 4px -2px'
-      );
   });
 });
