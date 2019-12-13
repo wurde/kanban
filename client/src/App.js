8b284e31552fe5c59eab9c2d42fc8d9ff8d43d5e
Andy Bettisworth <andy@accreu.com>
2019-12-13 14:14:16 -0600

    Cleanup App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,18 @@
  * Dependencies
  */
 
-import React, { useState } from 'react';
-import { Row, Column } from '@wurde/components';
-import { Board, CardModal } from './components';
+import React from 'react';
+import Board from './components/Board';
 
 /**
  * Define component
  */
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  function toggleModal() {
-    setIsModalOpen(!isModalOpen);
-  }
-
   return (
-    <Row className="App">
-      <Column>
-        <Board toggleModal={toggleModal} />
-        <CardModal isModalOpen={isModalOpen} toggleModal={toggleModal} />
-      </Column>
-    </Row>
+    <div className="App">
+      <Board />
+    </div>
   );
 }
 
